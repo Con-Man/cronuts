513739c2f4523bb7a53e6f5d2823eb763bcb525e
Con-Man <Cshocrylas@gmail.com>
2014-10-12 16:08:58 -0300

    test

---
+++ wp-content/themes/arcade-basic/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the main and #page div elements.
+ *
+ * @since 1.0.0
+ */
+$bavotasan_theme_options = bavotasan_theme_options();
+?>
+	</main><!-- main -->
+
+	<footer id="footer" role="contentinfo">
+		<div id="footer-content" class="container">
+			<div class="row">
+				<div class="copyright col-lg-12">
+					<span class="pull-left"><?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'arcade' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
+					
+
+
+
+					<span class="credit-link pull-right"><a href="http://ca.linkedin.com/pub/connor-shocrylas/38/2bb/b8b"><i class="fa fa-bolt fa-2x"></i> Designed by Connor Shocrylas</a></span>
+				</div><!-- .col-lg-12 -->
+			</div><!-- .row -->
+		</div><!-- #footer-content.container -->
+	</footer><!-- #footer -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/arcade-basic/page-templates/template-videos.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: Videos
+ *
+ */
+
+get_header(); 
+?>
+<div class="primary-content">
+
+	<div class="row">
+			<div id="primary" class="col-md-12 hfeed">
+				<div class="page-header clearfix">
+					<h1 class="pull-left"><i class="fa fa-video-camera"></i>&nbsp;<?php the_title(); ?></h1>
+					</br></br>
+
+					<!-- Displays Content -->
+				
+					<?php if (have_posts()) : while (have_posts()) : the_post();?>
+					<?php the_content(); ?>
+					<?php endwhile; endif; ?>
+					
+
+		
+				<?php
+
+				// check if the repeater field has rows of data
+				if( have_rows('videos') ):
+
+				 	// loop through the rows of data
+				    while ( have_rows('videos') ) : the_row();
+				?>
+				<div class=" videos col-md-6">
+				        <!-- display a sub field value -->
+						<div class="video_name">
+						<h1>
+							<?php
+							 $video_name = the_sub_field('video_name');
+							 ?>
+						</h1>
+						 </div>
+
+						 <div class="youtube_video">
+							<?php 
+							$video_name = the_sub_field('youtube_embed_code');
+							?>
+						</div>
+				</div>		
+				<?php
+				    endwhile;
+
+				else :
+
+				    // no rows found
+
+				endif;
+
+				?>			
+
+	</div>
+
+
+
+
+
+
+</div>
+<?php
+
+get_footer();
